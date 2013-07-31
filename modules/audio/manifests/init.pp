ab0390b8ab0ef2d40fc10f68c39793b4cdeb4247
Chris Lowis <chris.lowis@gmail.com>
2013-07-31 17:21:37 +0100

    remove umute

--- modules/audio/manifests/init.pp
+++ modules/audio/manifests/init.pp
@@ -9,12 +9,6 @@ class audio {
     require => Package['mpd'],
   }
 
-  exec { "umute":
-    command => "amixer set -c 0 Master 70 unmute && amixer set -c 0 PCM 70 unmute",
-    path    => "/usr/bin/",
-    require => Package['alsa-utils'],
-  }
-
   file { "/music":
     ensure => "directory",
     owner  => "mpd",
