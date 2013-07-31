7e358d3d6a0b1deadf1e7fbb3ecb0c59369a8102
Chris Lowis <chris.lowis@gmail.com>
2013-07-31 16:23:44 +0100

    Initial commit of network config

---
+++ modules/network/manifests/init.pp
@@ -0,0 +1,30 @@
+class network {
+  package {'isc-dhcp-server': }
+
+  service { "isc-dhcp-server":
+    enable => false,
+    ensure => 'stopped',
+    require => Package['isc-dhcp-server']
+  }
+
+  file { "/etc/network/interfaces":
+    content => template("network/interfaces"),
+  }
+  file { "/etc/dhcp/dhcpd.conf":
+    content => template("network/dhcpd_conf"),
+    require => Package['isc-dhcp-server']
+  }
+  file { "/etc/default/isc-dhcp-server":
+    content => template("network/isc_dhcp_server"),
+    require => Package['isc-dhcp-server']
+  }
+  file { "/etc/rc.local":
+    content => template("network/rc_local"),
+  }
+  file { "/usr/local/bin/try_adhoc_network":
+    content => template("network/try_adhoc_network"),
+  }
+  file { "/etc/wpa_supplicant/wpa_supplicant.conf":
+    content => template("network/wpa_supplicant_conf"),
+  }
+}
