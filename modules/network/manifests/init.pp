01a8719f825a87978627bdf7d880e3b31fdcb3fa
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-08-27 21:14:29 +0100

    Allow configuring hostname by editing file in boot partition
    A file 'hostname.txt' in the boot partition will be copied to
    /etc/hostname if it's newer than the existing hostname file.
    
    This allows the hostname to be configured by editing the file
    on another machine.

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -15,4 +15,7 @@ class network {
   file { "/etc/wpa_supplicant/wpa_supplicant.conf":
     content => template("network/wpa_supplicant_conf"),
   }
+  file { "/boot/hostname.txt":
+    content => "radiodan",
+  }
 }
