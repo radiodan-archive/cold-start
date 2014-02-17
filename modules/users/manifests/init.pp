005099450ddc0472704a042caff24437ee441aa8
Dan Nuttall <pixelblend@gmail.com>
2014-02-17 17:01:21 +0000

    Add pi user to audio group (to play audio)

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -5,6 +5,7 @@ class users {
     password => '$6$unnvVux0$PI6kGlFOoI63KLETuB59x9jVZvy1y7DfrTKzHwD4bC/fBeyNqKwiG3Knu8k6vIXV6mLtjA0rKwQBJ18ioE8k20',
     # password => sha1('raspberry'),
     require  => File['/home/pi'],
+    groups => ['audio']
   }
 
   file { '/home/pi':
